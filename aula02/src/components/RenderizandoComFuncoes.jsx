b7b500e47189225f8d7fbd899180f0df2b8ee518
Daniel Araújo <danielhenriquesa14@gmail.com>
2025-09-10 14:25:00 -0300

    Projeto com aula01 e aula02

---
+++ aula02/src/components/RenderizandoComFuncoes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const RenderizandoComFuncoes = () => {
+    function oQueRenderizar(estado) {
+        if(estado === "PB"){
+            return<h1> João Pessoa</h1>
+        }   
+        else {
+        return <h2> Recife</h2>
+    }
+}
+  return (
+    <div>
+        {oQueRenderizar("PE")}
+        </div>
+  )
+}
+
+export default RenderizandoComFuncoes
---
+++ aula02/src/components/TrabalhandoComImagens.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Kuririn from'../assets/kuririn.png'
+const TrabalhandoComImagens = () => {
+  return (
+    <div>
+        <img src="goku.webp" alt="" />
+        <img src={Kuririn} alt="" />
+    </div>
+  )
+}
+
+export default TrabalhandoComImagens
