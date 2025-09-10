d3f80248577df6dcb4a3daeae1484a1944a749c1
Daniel Araújo <danielhenriquesa14@gmail.com>
2025-09-10 14:27:12 -0300

    Corrige aula01 (remove submódulo e adiciona arquivos normais)

---
+++ aula01/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PrecisoEstudar from './components/PrecisoEstudar'
+
+const App = () => {
+  return (
+    <div>
+      <PrecisoEstudar nomeDaTecnologia="React"/>
+    </div>
+  )
+}
+
+export default App
